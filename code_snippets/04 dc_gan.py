ce9d729542015f308d9db2433bf8cd7aec1337a1
mugoh <mugoh.ks@gmail.com>
2019-09-07 06:35:13 +0300

    create discriminator

--- code_snippets/04 dc_gan.py
+++ code_snippets/04 dc_gan.py
@@ -19,6 +19,7 @@ def load_dataset(batch_size=256):
     ).astype('float32')
 
     # Normalize images [-1, 1]
+    # (xi - .5*max[x])/ .5*max[x]
     train_images = (train_images - 127.5) / 127.5
     buffer_size = train_images.shape[0]
     train_dataset = tf.data.Dataset.from_tensor_slices(
@@ -72,3 +73,38 @@ class Generator(tf.keras.layers.Model):
                 x = getattr(self, f'x{i}')(x)
 
             return self.output_prediction(x)
+
+
+class Discriminator(tf.keras.models.Model):
+    """
+        Image Classifier
+    """
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.x0 = tf.keras.layers.Conv2D(filters=64,
+                                         kernel_size=5,
+                                         strides=2,
+                                         padding='same',
+                                         input_shape=[28, 28, 1])
+        self.x1 = tf.keras.layers.LeakyReLU()
+        self.x2 = tf.keras.layers.Dropout(0.3)
+
+        self.x3 = tf.keras.layers.Conv2D(filters=128,
+                                         kernel_size=5,
+                                         strides=2,
+                                         padding='same')
+
+        self.x4 = tf.keras.layers.LeakyReLU()
+        self.x5 = tf.keras.layers.Dropout(0.3)
+
+        self.x6 = tf.keras.layers.Flatten()
+        self.output_prediction = tf.keras.layers.Dense(1)
+
+    def call(self, inputs):
+        x = self.x0(inputs)
+
+        for i in range(1, 7):
+            x = getattr(self, f'x{i}')(x)
+
+        return self.output_prediction(x)
